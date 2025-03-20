dbd7c56507a19ddc89fb498325cfe2bf3525a154
levisonhilado906 <levisonhilado906@gmail.com>
2025-03-20 17:26:19 +0000

    Add 13.rs

---
+++ src/13.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Define variables
+    let a = 5;
+    let b = 10;
+
+    // Calculate multiplication
+    let result = a * b;
+
+    // Print result
+    println!("The product of {} and {} is {}", a, b, result);
+}
