b70dd3aa85ccc3439bf0bdcf9a835d4009bdfa1a
levisonhilado906 <levisonhilado906@gmail.com>
2025-03-22 15:48:39 +0000

    Add 14.rs

---
+++ src/14.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Example Rust code: Basic arithmetic operation
+
+    let num1 = 5;
+    let num2 = 3;
+
+    match num1 * num2 {
+        15 => println!("The product of {} and {} is 15", num1, num2),
+        _ => println!("Product is not 15"),
+    }
+}
