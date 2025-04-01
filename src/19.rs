6e340a51f35b0a0ff71ecfdb0b2fb0c71e7e8409
levisonhilado906 <levisonhilado906@gmail.com>
2025-04-01 16:25:47 +0000

    Add 19.rs

---
+++ src/19.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Example Rust code for an educational purpose only.
+    let x = 10;
+    if x > 5 {
+        println!("x is greater than 5.");
+    } else {
+        println!("x is not greater than 5.");
+    }
+}
