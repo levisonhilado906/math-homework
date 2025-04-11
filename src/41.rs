52388c7311b201226f108b6178d181aef9bf2179
levisonhilado906 <levisonhilado906@gmail.com>
2025-04-11 12:52:15 +0000

    Add 41.rs

---
+++ src/41.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // Define the operation to be performed
+    let operation = |x: i32, y: i32| x + y;
+    // Call the operation with two parameters
+    println!("{}", operation(5, 3));
+}
