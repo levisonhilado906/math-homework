cfe02a993f53a95b62721561c58cceccfc789a19
levisonhilado906 <levisonhilado906@gmail.com>
2025-04-11 12:35:25 +0000

    Add 32.rs

---
+++ src/32.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // Example of simple arithmetic operations
+    let sum = 2 + 3;
+    let difference = 7 - 2;
+
+    println!("The sum is: {}", sum);
+    println!("The difference is: {}", difference);
+
+    // Another example with conditional statements
+    if true {
+        println!("This line will execute if the condition is true");
+    } else {
+        println!("This line will not execute because the condition is false");
+    }
+}
