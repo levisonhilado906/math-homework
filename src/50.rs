2d9e7682d61466a5c5104c98585d10c8c519dd3f
levisonhilado906 <levisonhilado906@gmail.com>
2025-04-11 14:10:50 +0000

    Add 50.rs

---
+++ src/50.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Example Rust code to demonstrate basic operations and functions
+    let x = 5;
+    println!("The value of x is: {}", x);
+
+    let y = "Hello, World!";
+    println!("The string 'Hello, World!' has been printed.");
+
+    // Additional examples and functions can be added here
+}
