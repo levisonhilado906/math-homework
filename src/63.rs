e643834ffd2b7340421df11ac02bc67cb86b86c3
levisonhilado906 <levisonhilado906@gmail.com>
2025-05-23 06:51:37 +0000

    Add 63.rs

---
+++ src/63.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // Example code: calculating the sum of two numbers
+    let number1 = 5;
+    let number2 = 3;
+    let result = number1 + number2;
+    println!("The sum is: {}", result);
+}
