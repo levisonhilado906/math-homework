a47153f147cb2456a82c44ca6c6c93c647129c1c
levisonhilado906 <levisonhilado906@gmail.com>
2025-05-27 12:55:09 +0000

    Add 66.rs

---
+++ src/66.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let number1 = 5;
+    let number2 = 3;
+
+    if number1 > number2 {
+        println!("Number 1 is greater than Number 2.");
+    } else {
+        println!("Number 2 is greater than Number 1.");
+    }
+}
